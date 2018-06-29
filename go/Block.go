63aa11c52e7b5a9740cef93e88fde5972d86491c
akhileshmaurya <maurya_akhilesh@ymail.com>
2018-06-29 09:50:17 +0530

    Adding block for blockchain

---
+++ go/Block.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+// Block keeps block headers
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
