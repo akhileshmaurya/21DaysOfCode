eb597aff9ad94bf25f792ccc5dc839e13cfa9598
akhileshmaurya <maurya_akhilesh@ymail.com>
2019-01-19 21:48:30 +0530

    Day-7 adding Some Sorting Classes

---
+++ src/main/java/com/learntocode/sorting/MergeSortItrative.java
@@ -0,0 +1,7 @@
+package com.learntocode.sorting;
+
+public class MergeSortItrative implements Sorting {
+
+  @Override
+  public void sort(int[] array) {}
+}
---
+++ src/main/java/com/learntocode/sorting/QuickSort.java
@@ -0,0 +1,7 @@
+package com.learntocode.sorting;
+
+public class QuickSort implements Sorting {
+
+  @Override
+  public void sort(int[] array) {}
+}
