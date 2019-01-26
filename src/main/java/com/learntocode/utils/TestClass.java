44ac660c8a0d9f51012864daa1098fbf14ab81f0
akhileshmaurya <maurya_akhilesh@ymail.com>
2019-01-26 20:31:17 +0530

    Adding Some Value

---
+++ src/main/java/com/learntocode/utils/TestClass.java
@@ -0,0 +1,12 @@
+package com.learntocode.utils;
+
+import java.util.Date;
+
+public class TestClass {
+  
+  public static void main(String[] args) {
+    Date date = new Date(2019, 02, 23, 23, 59, 60);
+    System.out.println(date);
+  }
+
+}
